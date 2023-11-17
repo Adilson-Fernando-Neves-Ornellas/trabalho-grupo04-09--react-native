60c3ff0c231ba54b72a3b519450ba89786957c48
Adilson Fernando Neves Ornellas <ornellas00@hotmail.com>
2023-11-17 15:15:43 -0300

    feat: criação do header da page Home

--- TrabalhoGrupo4/src/pages/Home/index.tsx
+++ TrabalhoGrupo4/src/pages/Home/index.tsx
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native'; 
 import styles from './styles';
+import Header from '../../components/header/header';
 
 const Home = () => {
 
   return (
     <View>
+      <Header />
       <Text style={styles.text}>PAGE HOME</Text>
     </View>
   );
