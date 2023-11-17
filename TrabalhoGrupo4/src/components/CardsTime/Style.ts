bd83f2105aa2864527b63e04243e14d61b602ca8
Adilson Fernando Neves Ornellas <ornellas00@hotmail.com>
2023-11-17 20:23:07 -0300

    feat: adicionando a estilização da pagina T|ime

---
+++ TrabalhoGrupo4/src/components/CardsTime/Style.ts
@@ -0,0 +1,26 @@
+import { StyleSheet } from 'react-native';
+
+const Styles = StyleSheet.create({
+  containerCards:{
+    margin: 30,
+    backgroundColor: "#1E1E1E",
+    borderRadius: 8
+ },
+  cardCarrosel:{
+    padding: 10,
+
+  },
+  div:{
+    marginBottom: 15,
+    marginLeft: 15,
+    display: "flex",
+    alignItems:'center',
+    justifyContent:'center'
+  },
+  textcard:{
+    color: "#D4BF00",
+    fontSize: 16,
+  }
+});
+
+export default Styles;
