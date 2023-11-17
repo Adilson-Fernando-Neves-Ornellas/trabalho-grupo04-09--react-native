801466437191f3651f750e331eef96c5573f5c4c
Raynan <raynan2007@gmail.com>
2023-11-17 20:02:12 -0300

    change:navegar pra tela cadastro a partir do login

--- TrabalhoGrupo4/src/pages/Cadastro/index.tsx
+++ TrabalhoGrupo4/src/pages/Cadastro/index.tsx
@@ -1,25 +1,28 @@
-
-import { View, Text, Image } from 'react-native'; 
-import styles from './styles';
-import { Input } from '../../components/Input'
-import { Button } from '../../components/Button'
-import { Footer } from '../../components/Footer';
-import { Logo } from '../../components/LogoSimples'
+import { View, Text, Image } from "react-native";
+import styles from "./styles";
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+import { Footer } from "../../components/Footer";
+import { Logo } from "../../components/LogoSimples";
 
 const Cadastro = () => {
-
   return (
     <View style={styles.viewContainer}>
       <View style={styles.viewContent}>
-        <Logo widthLogo={300} heightLogo={150}/>
-        <Input/>
-        <Input/>
-        <Input/>
-        <Input/>
-        <Button text='Cadastrar' buttonWidth={200} buttonHeight={60} textFontSize={36}/>
+        <Logo widthLogo={300} heightLogo={150} />
+        <Input />
+        <Input />
+        <Input />
+        <Input />
+        <Button
+          text="Cadastrar"
+          buttonWidth={200}
+          buttonHeight={60}
+          textFontSize={36}
+        />
         <Text style={styles.text}>Já possui uma conta? Entre!</Text>
       </View>
-      <Footer/>
+      <Footer />
     </View>
   );
 };
--- TrabalhoGrupo4/src/pages/Login/index.tsx
+++ TrabalhoGrupo4/src/pages/Login/index.tsx
@@ -11,10 +11,16 @@ const Login = () => {
   const verificarLogin = () => {
     navigation.navigate("home" as never);
   };
+
+  const cadastrar = () => {
+    navigation.navigate("cadastro" as never);
+  };
+
   return (
     <View style={styles.container}>
       <Text style={style.text}>PAGE LOGIN</Text>
       <Button color={"#8B0000"} title="Entrar" onPress={verificarLogin} />
+      <Button color={"#8B0000"} title="Cadastrar" onPress={cadastrar} />
     </View>
   );
 };
