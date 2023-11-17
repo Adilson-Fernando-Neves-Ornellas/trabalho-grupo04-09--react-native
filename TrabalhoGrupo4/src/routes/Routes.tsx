be2c8e0b905903675e00433a1a4b164b9e706daa
Adilson Fernando Neves Ornellas <ornellas00@hotmail.com>
2023-11-17 19:20:32 -0300

    fix: erro de importalão nas rotas

--- TrabalhoGrupo4/src/routes/Routes.tsx
+++ TrabalhoGrupo4/src/routes/Routes.tsx
@@ -1,6 +1,7 @@
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import StackRoutes from "./Stack/StackRoutes";
 export default function Routes() {
+  
 
   return (
     <NavigationContainer theme={DefaultTheme}>
