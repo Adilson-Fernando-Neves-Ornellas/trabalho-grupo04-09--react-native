6007b0ff26a80d50f4faabec1023f33cba757604
Tayaguiar <tatazinhamello@hotmail.com>
2023-11-20 17:03:04 -0300

    ajuste

--- TrabalhoGrupo4/src/pages/Login/styles.ts
+++ TrabalhoGrupo4/src/pages/Login/styles.ts
@@ -15,47 +15,18 @@ const styles = StyleSheet.create({
     height: 150,
     marginTop: 75,
   },
-  text: {
-    textAlign: "center",
-    fontSize: 18,
-  },
-  button: {
-    backgroundColor: 'blue',
-    paddingVertical: 15,
-    borderRadius: 20,
-    marginTop: 80,
-    marginBottom: 20, 
-    width: '50%', 
-  },
-
-  buttonText: {
-    color: '#d4bf00',
-    textAlign: 'center',
-    fontSize: 18,
-    fontFamily: '?',
-  },
   signupText: {
     color: colors.yellowPrimary,
     marginTop: 10,
+    fontFamily: fonts.texto
   },
   viewContent: {
     paddingTop: 75,
     flexDirection: 'column',
     alignItems: 'center',
-  },
   
-  footer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    alignItems: 'center',
-    backgroundColor: 'transparent', 
   },
-  footerImage: {
-    width: '100%', 
-    height: 100, 
-  },
+
 });
 
 export default styles;
