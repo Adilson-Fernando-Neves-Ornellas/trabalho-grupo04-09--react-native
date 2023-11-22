b84465e26a8a43c48d8e6dfac718b010d53c3cd3
Raynan <raynan2007@gmail.com>
2023-11-22 18:55:56 -0300

    fix: provedor assemblecontext aplicado

--- TrabalhoGrupo4/src/components/Modal/index.tsx
+++ TrabalhoGrupo4/src/components/Modal/index.tsx
@@ -1,46 +1,44 @@
 import React, { useState, useContext } from "react";
 import { Text, View, Modal, Image, TouchableOpacity } from "react-native";
 import { styles } from "./styles";
-import AssembleImage from '../../assets/Images/assembleGif.gif'
-import { AntDesign } from '@expo/vector-icons'; 
+import AssembleImage from "../../assets/Images/assembleGif.gif";
+import { AntDesign } from "@expo/vector-icons";
 import colors from "../../styles/theme/colors";
 import { AssembleContext } from "../../Context/AssembleContext";
 
 interface ModalProps {
-    isModalVisible: boolean;
-    setIsModalVisible(value: boolean): void;
+  isModalVisible: boolean;
+  setIsModalVisible(value: boolean): void;
 }
 
-export const ModalAssemble = ({isModalVisible, setIsModalVisible,}: ModalProps) => {
-    const [isLoading, SetIsLoading] = useState<boolean>(true)
-    const { listaHerois } = useContext(AssembleContext);
+export const ModalAssemble = ({
+  isModalVisible,
+  setIsModalVisible,
+}: ModalProps) => {
+  const [isLoading, SetIsLoading] = useState<boolean>(true);
+  const { listaHerois } = useContext(AssembleContext);
 
-
-    return (
-            <Modal 
-                animationType="slide"
-                transparent={true}
-                visible={isModalVisible}
-                onRequestClose={() => {
-                    setIsModalVisible(false);
-                }}
-            >
-                <View style={styles.modal}>
-                    <View style={styles.containerModal}>
-                        <Image
-                            style={styles.image}
-                            source={AssembleImage}
-                        />
-                        <Text>{}</Text>
-                        <TouchableOpacity 
-                        onPress={() => { 
-                            setIsModalVisible(false);
-                            console.log(listaHerois)
-                        }}>
-                            <AntDesign name="close" size={24} color={colors.bluePrimary} />
-                        </TouchableOpacity>
-                    </View>
-                </View>
-            </Modal>
-    )
-}
+  return (
+    <Modal
+      animationType="slide"
+      transparent={true}
+      visible={isModalVisible}
+      onRequestClose={() => {
+        setIsModalVisible(false);
+      }}
+    >
+      <View style={styles.modal}>
+        <View style={styles.containerModal}>
+          <Image style={styles.image} source={AssembleImage} />
+          <TouchableOpacity
+            onPress={() => {
+              setIsModalVisible(false);
+            }}
+          >
+            <AntDesign name="close" size={24} color={colors.bluePrimary} />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </Modal>
+  );
+};
--- TrabalhoGrupo4/src/routes/BottomTab/BottomTabRoutes.tsx
+++ TrabalhoGrupo4/src/routes/BottomTab/BottomTabRoutes.tsx
@@ -1,62 +1,62 @@
-import {Image, Text} from "react-native";
+import { Image, Text } from "react-native";
 import Home from "../../pages/Home";
 import Time from "../../pages/Time";
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import TimeIcon from "../../assets/Images/teamButton.png"
-import HomeIcon from "../../assets/Images/heroesButton.png"
-import AboutImg from '../../assets/Images/aboutUsButton.png'
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import TimeIcon from "../../assets/Images/teamButton.png";
+import HomeIcon from "../../assets/Images/heroesButton.png";
+import AboutImg from "../../assets/Images/aboutUsButton.png";
 import About from "../../pages/About";
-
+import { AssembleProvider } from "../../Context/AssembleContext";
 
 const Tab = createBottomTabNavigator<RootTabParamList>();
 
 export type RootTabParamList = {
-	Home: {};
-	Time: {};
-	About: {};
-}
+  Home: {};
+  Time: {};
+  About: {};
+};
 
 export default function BottomTabRoutes() {
-
-    return (
-		<Tab.Navigator
-			screenOptions={{
-				headerShown: false,
-				tabBarStyle: { backgroundColor: '#5F0900', paddingBottom: 2 },
-				tabBarInactiveTintColor: '#aaa',
-				tabBarActiveTintColor: '#fff',
-				tabBarHideOnKeyboard:true,
-				
-			}}
-		>
-			<Tab.Screen
-				options={{
-					tabBarShowLabel:false,
-					tabBarIcon: ({ color }) => (
-						<Image
-							resizeMode='contain'
-							source={HomeIcon}
-							style={{width: 80,height: 80}}
-						/>
-					)
-				}}
-				name="Home"
-				component={Home}
-			/>
-			<Tab.Screen
-				options={{
-					tabBarShowLabel:false,
-					tabBarIcon: ({ color }) => (
-						<Image
-							resizeMode='contain'
-							source={TimeIcon}
-							style={{width: 80,height: 80}}
-						/>
-					)
-				}}
-				name="Time"
-				component={Time}
-			/>
-		</Tab.Navigator>
-	);
+  return (
+    <AssembleProvider>
+      <Tab.Navigator
+        screenOptions={{
+          headerShown: false,
+          tabBarStyle: { backgroundColor: "#5F0900", paddingBottom: 2 },
+          tabBarInactiveTintColor: "#aaa",
+          tabBarActiveTintColor: "#fff",
+          tabBarHideOnKeyboard: true,
+        }}
+      >
+        <Tab.Screen
+          options={{
+            tabBarShowLabel: false,
+            tabBarIcon: ({ color }) => (
+              <Image
+                resizeMode="contain"
+                source={HomeIcon}
+                style={{ width: 80, height: 80 }}
+              />
+            ),
+          }}
+          name="Home"
+          component={Home}
+        />
+        <Tab.Screen
+          options={{
+            tabBarShowLabel: false,
+            tabBarIcon: ({ color }) => (
+              <Image
+                resizeMode="contain"
+                source={TimeIcon}
+                style={{ width: 80, height: 80 }}
+              />
+            ),
+          }}
+          name="Time"
+          component={Time}
+        />
+      </Tab.Navigator>
+    </AssembleProvider>
+  );
 }
